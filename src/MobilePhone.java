8e4026ab91b3366771ac39110948c493f4977937
NRamm99 <162293775+NRamm99@users.noreply.github.com>
2025-10-14 21:48:30 +0200

    Initial commit // Opgave 1 done

---
+++ src/MobilePhone.java
@@ -0,0 +1,11 @@
+public class MobilePhone {
+    private String brand;
+    private String model;
+    private double price;
+
+    public MobilePhone(String brand, String model, double price){
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+}
